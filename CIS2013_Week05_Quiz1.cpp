af0659f11456b5ccc960d8748892f0815b2c9a2b
JaminROCK <KingJamin@hotmail.com>
2018-02-14 18:45:43 -0600

    Update CIS2013_Week05_Quiz1.cpp
    added more explicit comments and organization of whitespace..

--- CIS2013_Week05_Quiz1.cpp
+++ CIS2013_Week05_Quiz1.cpp
@@ -9,23 +9,26 @@ int primes[25];
 //Extra code for creating an array
 void setPrimeArray()
 {
+	
 	for (int i=2; i<100; i++) 
 	{
 		for (int j=2; j<i; j++)
-        	{
-	        	if (i % j == 0) 
+    	{
+	        if (i % j == 0) 
 			{
-		                break;
+		        break;
 			}
 			else if (i == j+1)
 			{
-		                //cout << i << " ";
+		        //cout << i << " ";
 				counter++; 
 				primes[counter] = i;
 			}
 		}
-        }
+    }
+
 }
+
 //Extra code for displaying the created array
 void displayPrimeArray()
 {
@@ -34,6 +37,7 @@ void displayPrimeArray()
 		cout << primes[i] << ", ";
 	}
 }
+
 //Utilized code that prints all the prime numbers between the first and second input
 void printAllPrimeNumbers(int firstNumber, int secondNumber)
 {
@@ -41,6 +45,7 @@ void printAllPrimeNumbers(int firstNumber, int secondNumber)
 	{
 		firstNumber = 2;
 	}
+
 	for (int i=firstNumber; i<secondNumber; i++) 
 	{
 		if(i == 2)
@@ -48,41 +53,44 @@ void printAllPrimeNumbers(int firstNumber, int secondNumber)
 			cout << i << " ";
 		}
 		for (int j=firstNumber; j<i; j++)
-        	{
-	        	if (i % j == 0) 
+        {
+	        if (i % j == 0) 
 			{
-		                break;
+		        break;
 			}
-			else if (i == j+1)
+			else if (i == j + 1)
 			{
-		                cout << i << " ";
+		        cout << i << " ";
 				//counter++; 
 				//primes[counter] = i;
 			}
 		}
-        }
-}
-//lcm function courtesy of https://www.sitesbay.com/cpp-program/cpp-find-lcm-of-two-numbers
-void lcm(int a,int b)
-{
-    int m,n;
-
-    m=a;
-    n=b;
-
-    while(m!=n)
-    {
-	if(m < n)
-	{
-	m=m+a;
-	}
-	else
-	{
-	    n=n+b;
-	    }
     }
 
-    cout<<"\nL.C.M. of "<<a<<" and "<<b<<" is "<<m;
+}
+
+//LCM algorithm
+void lcm(int firstNumber, int secondNumber)
+{
+
+    int x, y;
+
+    x = firstNumber;
+    y = secondNumber;
+
+    while(x != y)
+    {
+		if(x < y)
+		{
+			x += firstNumber;
+		}
+		else
+		{
+		    y += secondNumber;
+		}
+	}
+
+    cout << "\nL.C.M. of " << firstNumber << " and " << secondNumber << " is " << x;
 }
 
 //Main Function
@@ -90,7 +98,7 @@ int main()
 {
 	int firstNumber, secondNumber;
 	cout << "Enter first number: "; cin >> firstNumber;
-	cout << "\nEnter second number: "; cin >> secondNumber;
+	cout << "Enter second number: "; cin >> secondNumber;
 	
 	cout << "You entered: " << firstNumber << ", " << secondNumber << endl;
 	cout << "The prime numbers between those two numbers (including themselves if they are prime): " << endl;
